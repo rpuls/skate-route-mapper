66d2ed6807ff3f1933721891e8bfad47bafc628a
Timm Rasmussen <timm.rasmussen@gmail.com>
2026-05-08 01:29:02 +0200

    Hmm shared/design ??? Rasmus?

--- mobile/src/screens/RideDetailScreen.tsx
+++ mobile/src/screens/RideDetailScreen.tsx
@@ -13,7 +13,7 @@ import dayjs from "dayjs";
 
 import { getRide, getSamplesForRide } from "../database/db";
 import RideRouteMap from "../components/RideRouteMap";
-import { colors, radius, shadows, space } from "@skate-route-mapper/shared";
+import { colors, radius, shadows, space } from "@skate-route-mapper/shared/design";
 import type { MeasurementSample } from "../types/measurement";
 
 type RouteParams = {
